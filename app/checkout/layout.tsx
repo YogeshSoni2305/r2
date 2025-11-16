b5ddd045212156ee728fa90e740e1158241bdacf
YogeshSoni2305 <yogeshsoni233005@gmail.com>
2025-11-16 16:05:59 +0530

    d1

---
+++ app/checkout/layout.tsx
@@ -0,0 +1,5 @@
+"use client";
+
+export default function CheckoutLayout({ children }: { children: React.ReactNode }) {
+  return <>{children}</>;
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,35 +1,5 @@
 
 
-
-// import "./globals.css";
-// import { ReactNode } from "react";
-// import Header from "@/components/Header";
-// import Footer from "@/components/Footer";
-// import { CartProvider } from "@/context/CartContext";
-
-// export const metadata = {
-//   title: "Ratnaasya – Buy Any 5 for ₹999",
-//   description: "Jewellery that celebrates you — Buy 5 for ₹999, 10 for ₹1500.",
-// };
-
-// export default function RootLayout({ children }: { children: ReactNode }) {
-//   return (
-//     <html lang="en">
-//       <body className="bg-white text-gray-900 flex flex-col min-h-screen">
-//         <CartProvider>
-//           <Header />
-//           <main className="flex-1 w-full">
-
-//             {children}
-//           </main>
-//           <Footer />
-//         </CartProvider>
-//       </body>
-//     </html>
-//   );
-// }
-
-
 import "./globals.css";
 import { ReactNode } from "react";
 import ClientWrapper from "../components/ClientWrapper";
